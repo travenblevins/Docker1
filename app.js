e03d805cc33a24cdb0355a05d4d84d01128b52a0
Traven <TRAVEN.BLEVINS9061@STU.MTEC.EDU>
2025-01-28 09:10:22 -0700

    local host issue fixed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path'); // Built-in Node.js module for handling file paths
 
 // Constants
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 
 // App
